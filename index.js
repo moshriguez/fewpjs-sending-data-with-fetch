f0bb8c9325c1f90562e27933a69601f8ff41fa0e
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-04-12 22:03:46 -0700

    commit tree eb7e3f1b2762915004a1a2dc6fcbb55bbfc395dc

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Add your code here
+// let myName = 'Marc'
+// let email = 'marc@gmail.com'
+
+function submitData(userName, userEmail) {
+    const body = document.querySelector('body')
+    const p = document.createElement('p')
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+            })
+        })
+    .then(resp => resp.json())
+    .then(json => {
+        p.textContent = json.id
+        body.appendChild(p)
+        console.log(json)
+        // return json
+    })
+    .catch((error) => {
+        console.log(error.message)
+        p.textContent = error.message
+        body.appendChild(p)
+    })
+}
+//submitData(myName, email);
